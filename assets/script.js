c041deea7a9a3cb7dc6867dee98a4d7fcbbf12bf
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-27 10:36:01 -0700

    scrolltotop and more articles

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
-function articles() {
-   var articlesDropdown = document.getElementById('dropdown');
-   if(articlesDropdown.style.display === '') {
-      articlesDropdown.style.display = 'block'
-   } else {
-      articlesDropdown.style.display = ''
+var scrollBtn = document.getElementById('topscroll')
+
+window.onscroll = () => {
+   scrollFunction()
+}
+
+function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollBtn.style.display = 'block'
+   }
+   else {
+      scrollBtn.style.display = 'none'
    }
 }
-function outsideClick() {
-   var articlesDropdown = document.getElementById('dropdown');
-   if(articlesDropdown.style.display === 'block') {
-      articlesDropdown.style.display = ''
-   }
+
+function scrollToTop() {
+   document.body.scrollTop = 0 // For Safari
+   document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
