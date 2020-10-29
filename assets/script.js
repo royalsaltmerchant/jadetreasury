ba6a53c8ab38ed98ec9cf30bafc1bbed41736d28
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-29 16:40:07 -0700

    scroll progress

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,33 @@
 var scrollBtn = document.getElementById('topscroll')
+var progBar = document.getElementById('myBar')
+var ProgCon = document.getElementById('progress-container')
+
 
 window.onscroll = () => {
    scrollFunction()
 }
 
 function scrollFunction() {
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+   var scrolled = (winScroll / height) * 100;
+
+   document.getElementById("myBar").style.width = scrolled + "%";
+   
+   if (winScroll > 20) {
       scrollBtn.style.display = 'block'
    }
    else {
       scrollBtn.style.display = 'none'
+   }
+
+   if(winScroll > 200) {
+      progBar.style.display = 'block'
+      ProgCon.style.display = 'block'
+   }
+   else {
+      progBar.style.display = 'none'
+      ProgCon.style.display = 'none'
    }
 }
 
