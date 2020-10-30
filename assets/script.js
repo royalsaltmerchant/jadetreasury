2d7a2893790bffb5ddd54b47d99a3e076486bcc0
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-29 19:31:58 -0700

    breadcrumbs for blog area

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
+// scroll functions
 var scrollBtn = document.getElementById('topscroll')
 var progBar = document.getElementById('myBar')
 var ProgCon = document.getElementById('progress-container')
-
 
 window.onscroll = () => {
    scrollFunction()
@@ -24,10 +24,12 @@ function scrollFunction() {
    if(winScroll > 200) {
       progBar.style.display = 'block'
       ProgCon.style.display = 'block'
+      locationDiv.style.display = 'block'
    }
    else {
       progBar.style.display = 'none'
       ProgCon.style.display = 'none'
+      locationDiv.style.display = 'none'
    }
 }
 
@@ -35,3 +37,15 @@ function scrollToTop() {
    document.body.scrollTop = 0 // For Safari
    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
+
+//location functions
+var string = document.location.href;
+var arrayOfStrings = string.toString().split('/');
+var myArray = arrayOfStrings[4].toString().split('.');
+console.log(arrayOfStrings)
+var myLocation = myArray[0]
+console.log(myLocation)
+
+var locationDiv = document.getElementById('location')
+locationDiv.innerHTML = `Blog --> ${myLocation}`
+
